614cadadabb2ec3cb53fd2803061d8b7f94ffa3f
Lucas <lucas.manfredh@telia.com>
2018-12-03 16:11:23 +0100

    Added cnn model

--- source/networkModel.py
+++ source/networkModel.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, UpSampling2D
 
 
 # Simple feed forward NN
@@ -21,7 +21,17 @@ def def_model_deep_ff():
     model.add(Dense(784, activation='sigmoid'))
     return model
 
-# Convolutional?
-# def def_model_cnn():
+# Convolutional
+def def_model_cnn():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(28,28,1)))
+    model.add(MaxPooling2D((2, 2), padding='same'))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D((2, 2), padding='same'))
 
-
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(1, (3, 3), activation='sigmoid', padding='same'))
+    return model
